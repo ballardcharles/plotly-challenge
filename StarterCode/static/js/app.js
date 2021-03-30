ae510dbbc807c0868d82b1cc190224ce363f1ddf
ballardcharles <73306975+ballardcharles@users.noreply.github.com>
2021-03-30 18:24:23 -0400

    added code to display two plotly plots.

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,86 @@
+function showPlot(id) {
+
+    d3.json("../data/samples.json").then((data) => {
+        console.log(data)
+        
+        var samples = data.samples.filter(s => s.id.toString() === id)[0];
+
+        console.log(samples)
+
+        var topSamples = samples.sample_values.slice(0,10).reverse();
+
+        var valueId = (samples.otu_ids.slice(0,10)).reverse();
+
+        var otuId = valueId.map(d => "OTU " + d)
+
+        var labels = samples.otu_labels.slice(0,10).reverse();
+
+        var barTrace = {
+            x: topSamples,
+            y: otuId,
+            text: labels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        var barData = [barTrace];
+
+        var layout = {
+            title: "Top 10 OTU",
+            yaxis:{
+                tickmode:"linear",
+            },
+            margin: {
+                l: 100,
+                r: 50,
+                t: 30,
+                b: 20
+            }
+        };
+
+        Plotly.newPlot("bar", barData, layout);
+
+        var bubbleTrace = {
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            mode: 'markers',
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            },
+            text: samples.otu_labels
+        }
+
+        var bubbleData = [bubbleTrace]
+
+        var bubbleLayout = {
+            title: `Belly Sample ${id}`,
+            height: 600,
+            width: 1300
+        }
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout)
+    
+    });
+
+};
+
+function optionChanged(id) {
+    showPlot(id)
+};
+
+function init() {
+    var selection = d3.select("#selDataset");
+
+    d3.json("../data/samples.json").then((data) => {
+
+        data.names.forEach((name) => {
+            selection.append("option").text(name).property("value");
+        });
+
+        showPlot(data.names[0]);
+    });
+};
+
+init();
+
